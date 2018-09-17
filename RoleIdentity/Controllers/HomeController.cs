69abdc5b2fc0e9afd692c2ac767f4467027291c5
Fahad Sherwani <fahadsherwani01@outlook.com>
2018-09-17 12:05:22 +0500

    df

---
+++ RoleIdentity/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using RoleIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoleIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+           ApplicationDbContext context = new ApplicationDbContext();
+            ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin"))
+                                            .ToList(), "Name", "Name");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
